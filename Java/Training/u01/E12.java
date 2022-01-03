e57e2870f21be854901d847406e595c1bd59669c
diggerydoo68 <rammerjam64@gmail.com>
2022-01-03 13:55:03 -0500

    Create E12.java

---
+++ Java/Training/u01/E12.java
@@ -0,0 +1,23 @@
+package com.themisinc.u01;
+
+public class E12 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ryan\nSoftware Engineer");
+		//newMain();
+
+	}
+	// alternative method to print job title
+	public static void newMain() {
+		String name = "Ryan"; //variables
+		String jobTitle = "Software Engineer";		
+		System.out.println("My name: " + name);
+		System.out.println("My job title: " + jobTitle);
+		
+		System.out.println("Ryan\nSoftware Engineer");
+
+	}
+
+}
+  
