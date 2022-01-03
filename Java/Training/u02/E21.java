20965a61b4b8432d02dcb85931f53ce0f9184f80
diggerydoo68 <rammerjam64@gmail.com>
2022-01-03 13:56:06 -0500

    Create E21.java

---
+++ Java/Training/u02/E21.java
@@ -0,0 +1,14 @@
+package com.themisinc.u02;
+
+public class E21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i1 = 34;
+		int i2 = 567;
+		i1 = i1 + i2;
+		System.out.println("total=" + i1);
+
+	}
+
+}
