4214be2f2589f36cffb6d8096be9f451dc54c536
diggerydoo68 <rammerjam64@gmail.com>
2022-01-03 13:55:25 -0500

    Create E13.java

---
+++ Java/Training/u01/E13.java
@@ -0,0 +1,13 @@
+package com.themisinc.u01;
+
+public class E13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n\n\n\n\n\n");
+		System.out.println("\t\t\tRyan\n\t\t\tSoftware Engineer");
+		System.out.println("\n\n\n");
+
+	}
+
+}
