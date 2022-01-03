a39c16ea1ada4b266677aee25f72909e21156490
diggerydoo68 <rammerjam64@gmail.com>
2022-01-03 13:58:50 -0500

    Create P208.java

---
+++ Java/Training/u02/P208.java
@@ -0,0 +1,13 @@
+package com.themisinc.u02;
+
+public class P208 {
+
+	public static void main(String[] args) {
+		double d = 2.7;
+		float f = 1.5F;  //casting the literal 'F' is required
+		
+		System.out.println("d=" + (d + f));
+
+	}
+
+}
