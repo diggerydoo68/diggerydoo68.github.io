4cfdf024e8b5174d374194c2b51bc9e8c4aa8273
diggerydoo68 <rammerjam64@gmail.com>
2022-01-03 13:58:20 -0500

    Create P206.java

---
+++ Java/Training/u02/P206.java
@@ -0,0 +1,14 @@
+package com.themisinc.u02;
+
+public class P206 {
+
+	public static void main(String[] args) {
+		char c1 = 'A';  //character A
+		char c2 = ' ';  //space aka blank character
+		char c3 = '\''; //for a single quote
+		
+		System.out.println(c1 + "-" + c2 + '-' + c3);
+
+	}
+
+}
