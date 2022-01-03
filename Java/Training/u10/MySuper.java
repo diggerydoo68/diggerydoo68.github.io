322ffb7ea0def61c24c15878f88b853f5e4fdb9b
diggerydoo68 <rammerjam64@gmail.com>
2022-01-03 14:23:33 -0500

    Create MySuper.java

---
+++ Java/Training/u10/MySuper.java
@@ -0,0 +1,13 @@
+package com.themisinc.u10;
+
+public class MySuper {
+    private int first;
+    
+    public MySuper (int first){
+    	this.first=first;
+    }
+    
+    public int getFirst (){
+    	return first;
+    }
+}
